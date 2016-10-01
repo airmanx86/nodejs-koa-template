9fd71f447fb9e528f6de61757ced5bf4cec01949
Arthur Ho <arthur_hohoho@hotmail.com>
2016-10-01 15:40:07 +1000

    refactor(app): Update packages and refactor some generator functions

--- app/routes/monitoringRoutes.js
+++ app/routes/monitoringRoutes.js
@@ -5,8 +5,9 @@ const koaRouter = require('koa-router');
 function monitoringRoutes() {
     const router = koaRouter();
 
-    router.get('/liveness', function* livenessHandler() {
+    router.get('/liveness', function* livenessHandler(next) {
         this.body = 'I am alive!';
+        yield next;
     });
 
     return router;
